eb0903b78bf46b7855a4b926d6274051ee95b4bb
Jason Smalridge <jasons@unity3d.com>
2019-11-05 10:19:43 -0500

    inital commit with clion new project

---
+++ cmake-build-debug/CMakeFiles/morphene.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/morphene.dir/main.cpp.o"
+  "morphene"
+  "morphene.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/morphene.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
